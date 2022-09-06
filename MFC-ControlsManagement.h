faebcf1d37e6aecf719cf2c8946caddbd5e92c07
Myoungmin <miung222@gmail.com>
2022-09-06 22:41:35 +0900

    Add project files.

---
+++ MFC-ControlsManagement.h
@@ -0,0 +1,32 @@
+﻿
+// MFC-ControlsManagement.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCControlsManagementApp:
+// See MFC-ControlsManagement.cpp for the implementation of this class
+//
+
+class CMFCControlsManagementApp : public CWinApp
+{
+public:
+	CMFCControlsManagementApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCControlsManagementApp theApp;
---
+++ MFC-ControlsManagementDlg.h
@@ -0,0 +1,33 @@
+﻿
+// MFC-ControlsManagementDlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCControlsManagementDlg dialog
+class CMFCControlsManagementDlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCControlsManagementDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCCONTROLSMANAGEMENT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
