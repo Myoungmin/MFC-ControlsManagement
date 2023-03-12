0702b39562e92f14e8a5ff612d56a37c9722054c
AzureAD\고명민 <miung222@gmail.com>
2023-03-12 20:59:13 +0900

    radio button 추가

--- Resource.h
+++ Resource.h
@@ -3,11 +3,16 @@
 // Used by MFCControlsManagement.rc
 //
 #define IDD_MFCCONTROLSMANAGEMENT_DIALOG 102
-#define IDD_CRadioButtonDialog          103
 #define IDR_MAINFRAME                   128
 #define IDD_RADIO_BUTTON_DIALOG         130
 #define IDC_CHECK1                      1000
 #define IDC_EDIT1                       1001
+#define IDC_RADIO1                      1005
+#define IDC_RADIO2                      1006
+#define IDC_RADIO3                      1007
+#define IDC_RADIO4                      1008
+#define IDC_RADIO5                      1009
+#define IDC_RADIO6                      1010
 
 // Next default values for new objects
 // 
@@ -15,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
